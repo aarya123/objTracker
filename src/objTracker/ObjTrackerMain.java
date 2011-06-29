2ae5e5923c798afe51b5a0ad89df003449614db1
aarya123 <anubhaw.arya@gmail.com>
2011-06-28 20:03:47 -0700

    More running and testing

--- src/objTracker/ObjTrackerMain.java
+++ src/objTracker/ObjTrackerMain.java
@@ -9,23 +9,24 @@ public class ObjTrackerMain {
 		for(int i=0;i<3;i++){
 			System.out.println("Enter side #"+(i+1));
 			sides[i]=scn.nextDouble();
-			System.out.println(sides[i]);
 		}
 		for(int i=0;i<3;i++){
 			System.out.println("Enter angle #"+(i+1));
 			angles[i]=scn.nextDouble();
-			System.out.println(angles[i]);
 		}
 		Triangle triangle = new Triangle(sides[0],sides[1],sides[2],angles[0],angles[1],angles[2]);
+                for(int i=0;i<3;i++){
+			System.out.println("Angle #"+(i+1));
+			System.out.println(triangle.getAngle(i));
+		}
 		for(int i=0;i<3;i++){
 			System.out.println("Side #"+(i+1));
 			System.out.println(triangle.getSide(i));
 		}
-		for(int i=0;i<3;i++){
-			System.out.println("Angle #"+(i+1));
-			System.out.println(triangle.getAngle(i));
-		}
-		
+                for(int i=0;i<3;i++){
+                    System.out.println("Height with side #"+(i+1)+" as the base");
+                    System.out.println(triangle.getHeight(i));
+                }
 	}
 
 }
