f2ce6998e1b4b2e9c5333d1105c89c6b86159a02
aarya123 <anubhaw.arya@gmail.com>
2011-07-02 10:42:50 -0700

    Hopefully the final version

--- src/objTracker/ObjTrackerMain.java
+++ src/objTracker/ObjTrackerMain.java
@@ -1,11 +1,11 @@
 package objTracker;
 import java.util.*;
-import objTracker.Triangle;
 public class ObjTrackerMain {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		double[] sides= new double[3];
 		double[] angles= new double[3];
+                System.out.println("-------Triangle 1-------");
 		for(int i=0;i<3;i++){
 			System.out.println("Enter side #"+(i+1));
 			sides[i]=scn.nextDouble();
@@ -14,22 +14,52 @@ public class ObjTrackerMain {
 			System.out.println("Enter angle #"+(i+1));
 			angles[i]=scn.nextDouble();
 		}
-		Triangle triangle = new Triangle(sides[0],sides[1],sides[2],angles[0],angles[1],angles[2]);
+		Triangle triangle1 = new Triangle(sides[0],sides[1],sides[2],angles[0],angles[1],angles[2]);
                 for(int i=0;i<3;i++){
 			System.out.println("Angle #"+(i+1));
-			System.out.println(triangle.getAngle(i));
+			System.out.println(triangle1.getAngle(i));
 		}
 		for(int i=0;i<3;i++){
 			System.out.println("Side #"+(i+1));
-			System.out.println(triangle.getSide(i));
+			System.out.println(triangle1.getSide(i));
 		}
                 for(int i=0;i<3;i++){
                     System.out.println("Height with side #"+(i+1)+" as the base");
-                    System.out.println(triangle.getHeight(i));
+                    System.out.println(triangle1.getHeight(i));
                 }
                 for(int i=0;i<3;i++){
                     System.out.println("Coordinates for point #"+(i+1));
-                    System.out.println("("+triangle.getCoordinates(i)[0]+", "+triangle.getCoordinates(i)[1]+")");
+                    System.out.println("("+CartCalc.getCoordinates(i, triangle1)[0]+", "+CartCalc.getCoordinates(i, triangle1)[1]+")");
+                }
+                System.out.println("-------Triangle 2-------");
+                for(int i=0;i<3;i++){
+			System.out.println("Enter side #"+(i+1));
+			sides[i]=scn.nextDouble();
+		}
+		for(int i=0;i<3;i++){
+			System.out.println("Enter angle #"+(i+1));
+			angles[i]=scn.nextDouble();
+		}
+                Triangle triangle2 = new Triangle(sides[0],sides[1],sides[2],angles[0],angles[1],angles[2]);
+                for(int i=0;i<3;i++){
+			System.out.println("Angle #"+(i+1));
+			System.out.println(triangle1.getAngle(i));
+		}
+		for(int i=0;i<3;i++){
+			System.out.println("Side #"+(i+1));
+			System.out.println(triangle1.getSide(i));
+		}
+                for(int i=0;i<3;i++){
+                    System.out.println("Height with side #"+(i+1)+" as the base");
+                    System.out.println(triangle1.getHeight(i));
+                }
+                for(int i=0;i<3;i++){
+                    System.out.println("Coordinates for point #"+(i+1));
+                    System.out.println("("+CartCalc.getCoordinates(i, triangle2)[0]+", "+CartCalc.getCoordinates(i, triangle2)[1]+")");
+                }
+                for(int i=0;i<3;i++){
+                    System.out.println("Distance between point #"+i);
+                    System.out.println(CartCalc.getDistance(triangle1, triangle2, i));
                 }
 	}
 }
